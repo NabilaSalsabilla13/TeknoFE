65d415d6f2c78e8d443ae9de80a43b2354b36881
bintang-bintang <bintangajiwibowo@gmail.com>
2024-04-30 07:52:25 +0700

    Penambahan browser route dan perbaikan route navigation navbar juga penambahan pages-pages untuk navigasi

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,49 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import Category from "./components/Category/Category";
-import Category2 from "./components/Category/Category2";
-import Services from "./components/Services/Services";
-import Banner from "./components/Banner/Banner"
-import Products from "./components/Products/Products";
-import headphone from "./assets/hero/headphone.png";
 import Footer from "./components/Footer/Footer";
-// import Routes from "./Routes";
+import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
+import About from "./pages/About";
+import Blog from "./pages/Catalog";
 
-const BannerData = {
-  discount: "30% OFF",
-  title: "Fine Smile",
-  image: headphone,
-  title2: "Air Solo Bass",
-  title3: "Winter Sale",
-  title4:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  bgColor: "#f42c37",
-};
-const BannerData2 = {
-  discount: "30% OFF",
-  title: "JJ Rimex",
-  image: headphone,
-  title2: "Air Solo Bass",
-  title3: "Winter Sale",
-  title4:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  bgColor: "#f42c37",
-};
+
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Category />
-      <Category2 />
-      <Banner data={BannerData} />
-      <Products />
-      <Services />
-      {/* <Routes /> */}
-      <Banner data={BannerData2} />
-      <Footer />
+      <BrowserRouter>
+        <Navbar /> {/** Navbar untuk semua page */}
+        <Routes> {/* Routes iki gawe penambahan link lek misal ono navigation iso ditambahno ndek Route ngisor iki */}
+          <Route index element={<Home />} />
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="about" element={<About />} />
+          <Route path="blog" element={<Blog />} />
+        </Routes>
+        <Footer /> {/** Footer untuk semua page */}
+      </BrowserRouter>
     </>
   )
 }
